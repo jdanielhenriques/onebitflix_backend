81889d28442bf88db204a00108278bfdf4d14406
José Daniel Henriques Porto <jdanielhenriques@gmail.com>
2023-05-29 10:04:23 +0100

    Created categories, courses and episodes tables. Created seeders.

---
+++ src/adminjs/resources/index.ts
@@ -0,0 +1,20 @@
+import { ResourceWithOptions } from "adminjs";
+import { Category, Course, Episode } from "../../models";
+import { categoryResourceOptions } from "./category";
+import { courseResourceOptions } from "./course";
+import { episodeResourceOptions } from "./episode";
+
+export const adminJsResources: ResourceWithOptions[] = [
+  {
+    resource: Course,
+    options: courseResourceOptions,
+  },
+  {
+    resource: Episode,
+    options: episodeResourceOptions,
+  },
+  {
+    resource: Category,
+    options: categoryResourceOptions,
+  },
+];
---
+++ src/models/index.ts
@@ -0,0 +1,15 @@
+// src/models/index.ts
+
+import { Category } from "./Category";
+
+import { Course } from "./Course";
+import { Episode } from "./Episode";
+
+Category.hasMany(Course);
+
+Course.belongsTo(Category);
+Course.hasMany(Episode);
+
+Episode.belongsTo(Course);
+
+export { Course, Category, Episode };
