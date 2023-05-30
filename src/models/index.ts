d44f905277cc9f262d43eb6c0bd7a92bba990db0
José Daniel Henriques Porto <jdanielhenriques@gmail.com>
2023-05-30 19:25:45 +0100

    Likes table and add like

--- src/models/index.ts
+++ src/models/index.ts
@@ -4,6 +4,7 @@ import { Category } from "./Category";
 import { Course } from "./Course";
 import { Episode } from "./Episode";
 import { Favorite } from "./Favorite";
+import { Like } from "./Like";
 import { User } from "./User";
 
 Category.hasMany(Course, { as: "courses" });
@@ -11,6 +12,7 @@ Category.hasMany(Course, { as: "courses" });
 Course.belongsTo(Category);
 Course.hasMany(Episode, { as: "episodes" });
 Course.belongsToMany(User, { through: Favorite });
+Course.belongsToMany(User, { through: Like });
 Course.hasMany(Favorite, { as: 'favoritesUser', foreignKey: 'course_id'})
 
 Episode.belongsTo(Course);
@@ -19,6 +21,7 @@ Favorite.belongsTo(Course);
 Favorite.belongsTo(User);
 
 User.belongsToMany(Course, { through: Favorite });
+User.belongsToMany(Course, { through: Like });
 User.hasMany(Favorite, { as: "favoritesCourses", foreignKey: "user_id" });
 
-export { Course, Category, Episode, User, Favorite };
+export { Course, Category, Episode, User, Favorite, Like };
--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,7 @@ import { episodesController } from "./controllers/episodesController";
 import { authController } from "./controllers/authController";
 import { ensureAuth, ensureAuthViaQuery } from "./middlewares/auth";
 import { favoritesController } from "./controllers/favoritesController";
+import { likesController } from "./controllers/likesController";
 
 const router = express.Router();
 
@@ -25,5 +26,6 @@ router.get("/favorites", ensureAuth, favoritesController.index);
 router.post("/favorites", ensureAuth, favoritesController.save);
 router.delete("/favorites/:id", ensureAuth, favoritesController.delete);
 
+router.post("/likes", ensureAuth, likesController.save);
 
 export { router };
